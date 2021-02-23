690b2b47aa30ab848b0d742b144dc0a97538e55e
vashchenkov <salam@aleikum>
2021-02-23 09:53:10 +0100

    ..

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <p style="text-align:left; padding: 1em">ChickClothes</p>
-    <b-nav tabs>
+    <b-nav tabs fill>
       <b-nav-item to="/" exact exact-active-class="active">Startseite</b-nav-item>
       <b-nav-item to="einkaufsbestellungen" exact exact-active-class="active">Einkaufsbestellungen</b-nav-item>
       <b-nav-item to="verkaufsauftraege" exact exact-active-class="active">Verkaufsaufträge</b-nav-item>
@@ -12,8 +12,12 @@
       <b-nav-item to="lieferanten" exact exact-active-class="active">Lieferanten</b-nav-item>
       <b-nav-item to="mitarbeiter" exact exact-active-class="active">Mitarbeiter</b-nav-item>
       <b-nav-item to="verkaufsstellen" exact exact-active-class="active">Verkaufsstellen</b-nav-item>
-      <b-nav-item to="suche" exact exact-active-class="active">Suche</b-nav-item>
-      <b-nav-item to="konto" exact exact-active-class="active">Konto</b-nav-item>
+      <b-nav-item to="suche" exact exact-active-class="active">
+        <b-icon-search></b-icon-search>
+      </b-nav-item>
+      <b-nav-item to="konto" exact exact-active-class="active">
+        <b-icon-settings></b-icon-settings>
+      </b-nav-item>
     </b-nav>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import VueEditableGrid from 'vue-editable-grid'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-editable-grid/dist/VueEditableGrid.css'
 
 Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVueIcons)
+Vue.component('vue-editable-grid', VueEditableGrid)
 
 Vue.config.productionTip = false
 
