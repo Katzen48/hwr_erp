76c2fb8a8c61923d56127baa6f3b15bfad1acb53
Katzen48 | Tobi <Katzen48@freenet.de>
2021-03-01 15:20:53 +0100

    added sanctum for authentication

--- routes/api.php
+++ routes/api.php
@@ -14,20 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('application/structure', [\App\Http\Controllers\API\Application\StructureController::class, 'routes']);
+//Route::middleware('auth:sanctum')->group(function () {
+    Route::get('application/structure', [\App\Http\Controllers\API\Application\StructureController::class, 'routes']);
 
-Route::group(['prefix' => 'scm'], function() {
-   Route::apiResource('items', \App\Http\Controllers\API\SCM\ItemController::class);
-   Route::apiResource('items.item_variants', \App\Http\Controllers\API\SCM\ItemVariantController::class);
-   Route::apiResource('storages', \App\Http\Controllers\API\SCM\StorageController::class);
-   Route::apiResource('outlets', \App\Http\Controllers\API\SCM\OutletController::class);
-   Route::apiResource('vendors', \App\Http\Controllers\API\SCM\VendorController::class);
-   Route::apiResource('purchase_headers', \App\Http\Controllers\API\SCM\PurchaseHeaderController::class);
-   Route::apiResource('purchase_headers.purchase_lines', \App\Http\Controllers\API\SCM\PurchaseLineController::class);
-   Route::apiResource('sales_headers', \App\Http\Controllers\API\SCM\SalesHeaderController::class);
-   Route::apiResource('sales_headers.sales_lines', \App\Http\Controllers\API\SCM\SalesLineController::class);
-});
+    Route::group(['prefix' => 'scm'], function() {
+        Route::apiResource('items', \App\Http\Controllers\API\SCM\ItemController::class);
+        Route::apiResource('items.item_variants', \App\Http\Controllers\API\SCM\ItemVariantController::class);
+        Route::apiResource('storages', \App\Http\Controllers\API\SCM\StorageController::class);
+        Route::apiResource('outlets', \App\Http\Controllers\API\SCM\OutletController::class);
+        Route::apiResource('vendors', \App\Http\Controllers\API\SCM\VendorController::class);
+        Route::apiResource('purchase_headers', \App\Http\Controllers\API\SCM\PurchaseHeaderController::class);
+        Route::apiResource('purchase_headers.purchase_lines', \App\Http\Controllers\API\SCM\PurchaseLineController::class);
+        Route::apiResource('sales_headers', \App\Http\Controllers\API\SCM\SalesHeaderController::class);
+        Route::apiResource('sales_headers.sales_lines', \App\Http\Controllers\API\SCM\SalesLineController::class);
+    });
 
-Route::group(['prefix' => 'administration'], function() {
-    Route::apiResource('employees', \App\Http\Controllers\API\Administration\EmployeeController::class);
-});
+    Route::group(['prefix' => 'administration'], function() {
+        Route::apiResource('employees', \App\Http\Controllers\API\Administration\EmployeeController::class);
+    });
+//});
